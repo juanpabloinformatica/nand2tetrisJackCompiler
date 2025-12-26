9b3917c6a95d70338f262bb2f592122f9cf1e058
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-12-26 17:42:29 +0100

    Feat: Adding instructions in JackAnalyzer module.
    Done:
    - Traverse functionalitie for files and directories
    - Started logic followed at the moment of parsing each file by using a
    JackTokenizer object

--- src/JackAnalyzer.cpp
+++ src/JackAnalyzer.cpp
@@ -1,16 +1,34 @@
-#include <JackAnalyzer.hpp>
+#include "JackAnalyzer.hpp"
 JackAnalyzer::JackAnalyzer(const std::string &filepath)
     : sourcePath(filepath) {}
-void JackAnalyzer::run() { 
-	if (!std::filesystem::exists(this->sourcePath))
-		return;
-	if(std::filesystem::is_regular_file(this->sourcePath))
-		this->handleFile();
-	if(std::filesystem::is_directory(this->sourcePath))
-	
+
+void JackAnalyzer::run() {
+
+  if (!std::filesystem::exists(this->sourcePath) == true) {
+    std::cout << "file doesn't exist" << "\n";
+    return;
+  }
+
+  std::filesystem::is_regular_file(this->sourcePath)
+      ? this->handleFile(this->sourcePath)
+      : this->handleFolder(this->sourcePath);
 }
-void JackAnalyzer::handleFile() {}
-void JackAnalyzer::handleFolder() {}
+void JackAnalyzer::handleFolder(const std::filesystem::path &folderFolder) {
+
+  for (const auto &dirEntry :
+       std::filesystem::recursive_directory_iterator(sourcePath)) {
+    if (dirEntry.is_regular_file())
+      this->handleFile(dirEntry.path());
+  }
+}
+void JackAnalyzer::handleFile(const std::filesystem::path &filePath) {
+  std::ifstream file(filePath);
+  JackTokenizer JackTokenizer = ::JackTokenizer(file);
+  JackTokenizer.run();
+  return;
+  /*Logic*/
+}
+
 JackAnalyzer::~JackAnalyzer() {
   std::cout << "Calling destructor" << std::endl;
 }
