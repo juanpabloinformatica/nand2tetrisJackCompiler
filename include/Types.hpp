3168b2b3eeaea009a935db60d64bb05df2a67c4c
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2026-02-04 22:51:43 +0100

    Feat: Adding common types to tokenizer and compilationEngine

--- include/Types.hpp
+++ include/Types.hpp
@@ -1,5 +1,8 @@
 #ifndef __TYPES__
 #define __TYPES__
+#include <algorithm>
+#include <regex>
+#include <string>
 class JackTypes {
 public:
   enum TokenType {
@@ -29,9 +32,31 @@ public:
     WHILE,
     RETURN,
     TRUE,
-    FLASE,
+    FALSE,
     _NULL,
     THIS
+  };
+
+  static inline bool tokenIsIntegerConstant(const std::string &token) {
+    return std::all_of(token.begin(), token.end(), ::isdigit);
+  }
+
+  static inline bool tokenIsStringConstant(const std::string &token) {
+    return token.at(0) == '"' && token.at(token.length() - 1) == '"';
+  }
+
+  static inline bool tokenIsKeywordConstant(const std::string &token) {
+    return token.compare("true") == 0 || token.compare("false") == 0 ||
+           token.compare("null") == 0 || token.compare("this") == 0;
+  }
+
+  static inline bool tokenIsUnaryOp(const std::string &token) {
+    return token == "-" || token == "~";
+  }
+  static inline bool tokenIsOp(const std::string &token) {
+    return token == "+" || token == "-" || token == "*" || token == "/" ||
+           token == "&" || token == "|" || token == "<" || token == ">" ||
+           token == "=";
   };
 };
 #endif
