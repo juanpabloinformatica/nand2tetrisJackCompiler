b41226543e90fa506af4c1f94e2cf8e5ff3fee12
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2026-02-24 16:32:41 +0100

    Feat: Adding Symboltable integration.
    DONE:
    - Implementing symboltable functions to store the different variables in
    class and subroutines scopes.
    - Calling SymbolTable module from CompilationEngine, when compiling
    varClassDec and ClassVarDec, SymbolsTables are populated.
    WHY:
    - Needed for following steps.

--- include/SymbolTable.hpp
+++ include/SymbolTable.hpp
@@ -1,25 +1,58 @@
 #ifndef __SYMBOL_TABLE__
 #define __SYMBOL_TABLE__
+#include <cassert>
+#include <map>
 #include <string>
-#include <vector>
+/*
+ * easier for looking up
+ * identifier : {.type = type, .kind=kind, .scope=scope}
+ * identifier : {.type = type, .kind=kind, .scope=scope}
+ * identifier : {.type = type, .kind=kind, .scope=scope}
+ * identifier : {.type = type, .kind=kind, .scope=scope}
+ * identifier : {.type = type, .kind=kind, .scope=scope}
+ * */
 class SymbolTable {
-
 public:
   SymbolTable();
-  void addSymbolTableObject(std::string name, std::string type,
-                            std::string kind, int scope);
-  void getSymbolTableObject(std::string name);
+  ~SymbolTable();
 
-private:
-  class SymbolTableObject {
+  class Symbol {
   public:
     std::string name;
-    std::string type;
-    std::string kind;
-    int scope;
-    SymbolTableObject(std::string name, std::string type, std::string kind,
-                      int scope);
+    std ::string type;
+    std ::string kind;
+    int posInSegment;
+    Symbol(std ::string name = "", std ::string type = "",
+           std ::string kind = "", int posInSegment = -1)
+        : name(name), type(type), kind(kind), posInSegment(posInSegment) {};
+    void setName(const std::string& name);
+    void setType(const std::string& type);
+    void setKind(const std::string& kind);
+    void setPosInSegment(int posInSegment);
   };
-  std::vector<SymbolTableObject> tableList;
+  std::map<std::string, Symbol> const& getSymbolTable();
+  Symbol* allocateSymbol();
+  void addSymbol(Symbol* symbol);
+  Symbol* getSymbol();
+  void setSymbol(Symbol* symbol);
+  void resetSymbolTable();
+
+private:
+  std::map<std::string, Symbol*> symbolTable;
+  Symbol* currentSymbol;
+  int staticSymbolCounter;
+  int fieldSymbolCounter;
+  int argumentSymbolCounter;
+  int localSymbolCounter;
+  void incrementSymbolCounter(const std::string& kind);
+  void setSymbolPosInSegment(Symbol* symbol);
+  void _deallocateSymbols();
 };
+/*I prefer composition*/
+// I do it after
+//  class SubroutineSymbolTable{
+//  public:
+//    SymbolTable
+//  };
+
 #endif
--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,3 +1,78 @@
 #include "SymbolTable.hpp"
+#include "CompilationEngine.hpp"
+#include <iostream>
+SymbolTable::SymbolTable()
+    : symbolTable(std::map<std::string, Symbol*>()), currentSymbol(nullptr),
+      staticSymbolCounter(-1), fieldSymbolCounter(-1),
+      argumentSymbolCounter(-1), localSymbolCounter(-1) {}
 
+/*Need to be destroy in table destructor*/
+void SymbolTable::setSymbol(Symbol* symbol) { this->currentSymbol = symbol; }
+SymbolTable::Symbol* SymbolTable::getSymbol() { return this->currentSymbol; }
+SymbolTable::Symbol* SymbolTable::allocateSymbol() {
+  return new SymbolTable::Symbol();
+}
+/*In this function I should handle the # field of the hashtable*/
+void SymbolTable::incrementSymbolCounter(const std::string& kind) {
+  if (kind == "static")
+    this->staticSymbolCounter++;
+  else if (kind == "field")
+    this->fieldSymbolCounter++;
+  else if (kind == "argument")
+    this->argumentSymbolCounter++;
+  else
+    this->localSymbolCounter++;
+  return;
+}
+void SymbolTable::addSymbol(SymbolTable::Symbol* symbol) {
+  assert(symbol->name != "");
+  this->incrementSymbolCounter(symbol->kind);
+  this->setSymbolPosInSegment(symbol);
+  this->symbolTable.insert({symbol->name, symbol});
+}
+void SymbolTable::setSymbolPosInSegment(Symbol* symbol) {
 
+  std::cout << "In here" << "\n";
+  if (symbol->kind == "static")
+    symbol->posInSegment = staticSymbolCounter;
+  else if (symbol->kind == "field")
+    symbol->posInSegment = fieldSymbolCounter;
+  else if (symbol->kind == "argument")
+    symbol->posInSegment = argumentSymbolCounter;
+  else
+    symbol->posInSegment = localSymbolCounter;
+  return;
+}
+void SymbolTable::Symbol::setName(const std::string& name) {
+  this->name = name;
+}
+void SymbolTable::Symbol::setKind(const std::string& kind) {
+  this->kind = kind;
+}
+void SymbolTable::Symbol::setType(const std::string& type) {
+  this->type = type;
+}
+
+void SymbolTable::Symbol::setPosInSegment(int posInSegment) {
+  this->posInSegment = posInSegment;
+}
+
+void SymbolTable::resetSymbolTable() {
+  this->_deallocateSymbols();
+  this->symbolTable.clear();
+  this->staticSymbolCounter = -1;
+  this->fieldSymbolCounter = -1;
+  this->argumentSymbolCounter = -1;
+  this->localSymbolCounter = -1;
+}
+
+void SymbolTable::_deallocateSymbols() {
+  /*Destroying each created symbol*/
+  for (auto symbol : this->symbolTable) {
+    delete symbol.second;
+  }
+}
+SymbolTable::~SymbolTable() {
+  /*Destroying each created symbol*/
+  this->_deallocateSymbols();
+}
