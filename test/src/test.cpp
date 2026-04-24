382a24a227f49f285f6b8be9a4ff9d94da16dc7d
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2026-04-24 15:53:07 +0200

    Feat: instruction added for passing test_6
    DONE:
    - Added some conditions in order to handle correctly arrays.
    - Added handling of null constant.
    TO_BE_DONE
    - Code quality is poorly a lot of repetition. Bad code

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,12 +1,41 @@
+#include <cassert>
 #include <iostream>
 #include <regex>
 int main() {
+  std::string expression = "a[b[[[[]]";
+  int value = std::count_if(expression.begin(), expression.end(),
+                            [](char letter) { return letter == '['; });
+  std::cout << "Value\t" << value;
 
-  std::string expression = "do Output.printString(Score: 0)";
-  bool hasAString =
-      (std::regex_search(expression, std::regex(R"(\".*\")"))) ? true : false;
-  if (hasAString)
-    std::cout << "matched";
-  else
-    std::cout << "NOT matched";
+  //
+  // std::string expression = "a[a[5]] * b[((7 - a[3]) - Main.double(2)) + 1]+";
+  // std::string right = expression.substr(expression.find_first_of("["),
+  //                                       expression.find_last_of("]") -
+  //                                           expression.find_first_of("[") +
+  //                                           1);
+  // const std::regex _operatorsPatternRegex(R"([\+|\~|\-|\*|\/|&|\||<|>|=])");
+  // std::regex arrayDelimeters = std::regex(R"(\[|\])");
+  // if (std::regex_search(expression, arrayDelimeters) &&
+  //     !std::regex_search(expression.substr(expression.find_last_of("]")),
+  //                        _operatorsPatternRegex) &&
+  //     !std::regex_search(expression.substr(0, expression.find_first_of("[")),
+  //                        _operatorsPatternRegex))
+  //   std::cout << "hereeee" << "\n";
+  //
+  // assert(true ==
+  //        std::regex_search(expression.substr(expression.find_last_of("]")),
+  //                          _operatorsPatternRegex));
+  // assert(true ==
+  //        std::regex_search(expression.substr(0,
+  //        expression.find_first_of("[")),
+  //                          _operatorsPatternRegex));
+  //
+  // std::string lef = expression.substr(expression.find_last_of("]"));
+  // std::string rig = expression.substr(0, expression.find_first_of("["));
+  //
+  // int firstOpeningP = expression.find_first_of('[');
+  // int lastClosingP = expression.find_last_of(']');
+  // std::cout << firstOpeningP << "\n";
+  // std::cout << lastClosingP << "\n";
+  // std::cout << right << "\n";
 }
